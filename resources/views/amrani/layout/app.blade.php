cd6780ef4b59f3b53d5aab7c114ac2c4c2f9f03c
extensionmedia <elmeftouhi@gmail.com>
2021-03-29 23:29:54 +0100

    Designing dashboard

--- resources/views/amrani/layout/app.blade.php
+++ resources/views/amrani/layout/app.blade.php
@@ -17,39 +17,16 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="" style="font-family: 'Nunito', sans-serif;">
-        <div class="h-full bg-gray-100">
-            <!-- Page Content -->
-            <div class="wrapper w-full h-full flex">
-
-                <div class="w-48 h-full bg-gradient-to-r from-gray-100 to-gray-200 border-r border-gray-300">
-                    <div class="flex items-center border-b border-gray-300 px-4 h-14">
-                        <img class="w-8 mr-2" src="http://adminmart.com/src/assets/images/logo-icon.png" alt="">
-                        <div class="text-gray-100 text-lg text-gray-800">
-                            <span class="font-bold text-sm">Agence</span> 
-                            <span class="text-sm">Amrani</span>
-                        </div>
-                    </div>
-                </div>
-
-
-                <main class="bg-white flex-1">
-                    <div class="flex items-center justify-between border-b border-gray-300 h-14">
-                        <div class="flex items-center">
-                            <button class="p-4 py-5 hover:bg-gray-100 text-lg"><i class="fas fa-bars"></i></button>
-                            <div class="rounded-lg border overflow-hidden relative">
-                                <input type="text" class="border-0 text-sm w-64" placeholder="Chercher">
-                                <button class="absolute top-0 right-0 m-2 pt-1 text-sm text-gray-400"><i class="fas fa-search"></i></button>
-                            </div>
-                        </div>
-                        <div class="flex items-center">
-                            <div class="rounded-full cursor-pointer hover:bg-red-300 p-2 bg-red-200 m-2 text-gray-500"><i class="far fa-bell"></i></div>
-                            <div class="rounded-full cursor-pointer hover:bg-gray-300 p-2 bg-gray-200 m-2"><i class="far fa-user"></i></div>
-                        </div>
-                    </div>
+        <div class="h-full bg-gray-100 overflow-hidden flex">
+                <!-- Vertical Menu -->
+                @include('amrani.layout.vertical_menu')
+                <!-- Page Content -->
+                <main class="bg-white flex-1 overflow-y-auto">
+                    <!-- Header -->
+                    @include('amrani.layout.header')
+                    <!-- Content -->
                     @yield('content')
-                </main>                
-            </div>
-
+                </main>
         </div>
     </body>
 </html>
---
+++ resources/views/amrani/layout/vertical_menu.blade.php
@@ -0,0 +1,61 @@
+<div class="flex flex-col justify-between min-w-48 w-48 h-full bg-gradient-to-r from-gray-100 to-gray-200 border-r border-gray-300 pb-4 overflow-y-auto">
+    <div class="">
+        <div class="flex items-center border-b border-gray-300 px-4 h-14 mb-2">
+            <img class="w-8 mr-2" src="http://adminmart.com/src/assets/images/logo-icon.png" alt="">
+            <div class="text-gray-100 text-lg text-gray-800">
+                <span class="font-bold text-sm">Agence</span> 
+                <span class="text-sm">Amrani</span>
+            </div>
+        </div>
+        <ul>
+            <li class="">
+                <a href="" 
+                    class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-100 text-sm bg-gray-600 rounded-r-full"
+                >                                
+                    <div class="w-6"><i class="fas fa-columns"></i> </div>
+                    Dashboard
+                </a>
+                <a href="" class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">                                
+                    <div class="w-6"><i class="fas fa-user-tie"></i> </div>
+                     Clients <small class="ml-2 text-xs font-bold">(45)</small>
+                </a>
+                <a href="" 
+                    class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">                                
+                    <div class="w-6"><i class="fas fa-user-friends"></i> </div>
+                    Intermédiaires <small class="ml-2 text-xs font-bold">(45)</small>
+               </a>
+                <a href="" class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">                                
+                    <div class="w-6"><i class="far fa-building"></i> </div>
+                     Appartements <small class="ml-2 text-xs font-bold">(96)</small>
+                </a>
+                <a href="" class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">                                <div class="w-6"><i class="fas fa-map-marked"></i> </div>
+                     Terrains <small class="ml-2 text-xs font-bold">(24)</small>
+                </a>
+                <a href="" class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">                                <div class="w-6"><i class="fas fa-home"></i> </div>
+                     Maison <small class="ml-2 text-xs font-bold">(12)</small>
+                </a>
+                <a href="" class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">                                <div class="w-6"><i class="fas fa-warehouse"></i> </div>
+                     Local Commercial <small class="ml-2 text-xs font-bold">(5)</small>
+                </a>
+                <a href="" class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">                                <div class="w-6"><i class="fas fa-home"></i> </div>
+                     Villas <small class="ml-2 text-xs font-bold">(2)</small>
+                </a>
+                <a href="" class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">                                <div class="w-6"><i class="fas fa-horse-head"></i> </div>
+                     Fermes <small class="ml-2 text-xs font-bold">(1)</small>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <ul>
+        <li>
+            <a href="" class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">
+                <div class="w-6"><i class="fas fa-users"></i> </div>
+                Utilisateurs
+           </a> 
+           <a href="" class="flex items-center cursor-pointer block hover:bg-gray-300 hover:text-gray-800 py-2 px-3 text-gray-600 text-sm">
+                <div class="w-6"><i class="fas fa-wrench"></i> </div>
+                Paramêtres
+       </a> 
+        </li>
+    </ul>
+</div>
