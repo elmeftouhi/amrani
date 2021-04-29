c74286ee8cb20c1066b89ff40e7f4ca4741912fa
extensionmedia <elmeftouhi@gmail.com>
2021-04-29 02:11:49 +0000

    dashboard

--- resources/views/amrani/dashboard/index.blade.php
+++ resources/views/amrani/dashboard/index.blade.php
@@ -7,12 +7,14 @@
             <h1 class="font-bold text-xl">Dashboard</h1>
         </div>
 
-        <div class="flex gap-4 px-4 py-6 flex-1 overflow-auto bg-gray-50">
-            <div class="w-2/3">
+        <div class="lg:flex gap-4 px-4 py-6 flex-1 overflow-auto bg-gray-50">
+            <div class="lg:w-1/3">
                 @include('amrani.dashboard.charts.bars')
             </div>
-            <div class="w-1/3">
+            <div class="lg:w-1/3">
                 @include('amrani.dashboard.charts.pie')
+            </div>
+            <div class="lg:w-1/3">
                 @include('amrani.dashboard.partials.logs')
             </div>
         </div>
