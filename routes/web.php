44b3cb268e7724629de064407db0ed2ef930ec33
elmeftouhi <elmeftouhi@gmail.com>
2023-02-12 21:46:35 +0100

    symblink router

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ use App\Models\City;
 use App\Models\Maison;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/symblink', function () {
+    Artisan::call('storage:link');
+});
+
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
 
 Route::resource('client', ClientController::class);
